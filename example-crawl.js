0b4f851daa51a064ebdaec9f6d43aa3ad0b9fb10
Mike Davies <isofarro2@gmail.com>
2015-02-23 08:59:39 +0000

    Leave second on listener to prove that on is chainable.

--- example-crawl.js
+++ example-crawl.js
@@ -5,6 +5,9 @@ crawler
     .startUrl('http://mainlytea.com/')
     .on('externalLink', function(link) {
         console.log("External Link: " + link.href);
+    })
+    .on('internalLink', function(link) {
+        console.log("Internal Link: " + link.href);
     });
 
 console.log("Crawler configured");
