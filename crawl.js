f4dae37136a91ffc19773381825a2a0a6cb9b5da
Mike Davies <isofarro2@gmail.com>
2015-02-23 20:30:50 +0000

    Start crawl and get links from the incoming document.

--- crawl.js
+++ crawl.js
@@ -1,4 +1,6 @@
-var events = require('events');
+var events = require('events'),
+    request = require('request'),
+    cheerio = require('cheerio');
 
 
 function Crawler() {
@@ -8,6 +10,10 @@ function Crawler() {
     }
 
     events.EventEmitter.call(self);
+    self.config = {};
+
+    self.queue    = [];
+    self.urlCache = [];
 
     process.nextTick(function() {
         self.start();
@@ -18,16 +24,83 @@ Crawler.prototype.__proto__ = events.EventEmitter.prototype;
 
 Crawler.prototype.startUrl = function(startUrl) {
     this.startUrl = startUrl;
+    this.queue.push(startUrl);
+    return this;
+}
+
+Crawler.prototype.addUrl = function(url) {
+    if (this.urlCache.indexOf(url) !== -1) {
+        this.queue.push(url);
+        this.urlCache.push(url);
+    }
+
     return this;
 }
 
 Crawler.prototype.start = function() {
+    var self = this;
     console.log("Starting crawler");
+    console.log("Queue length: " + this.queue.length)
 
-    // Testing event emitters are working as expected
-    this.emit('externalLink', {href: 'http://www.google.com/'});
-    this.emit('internalLink', {href: 'http://www.mainlytea.com/'});
+    // // Testing event emitters are working as expected
+    // this.emit('externalLink', {href: 'http://www.google.com/'});
+    // this.emit('internalLink', {href: 'http://www.mainlytea.com/'});
+
+    process.nextTick(function() {
+        self._crawl();
+    });
 }
+
+Crawler.prototype._crawl = function() {
+    var self = this,
+        nextUrl = self.queue.shift();
+
+    self._getUrl(nextUrl, function() {
+        console.log("Fetched: " + nextUrl);
+
+        // Next URL
+        if (self.queue.length) {
+            process.nextTick(function() {
+                self._crawl();
+            })
+        } else {
+            console.log("Queue empty. Complete");
+        }
+    });
+}
+
+Crawler.prototype._getUrl = function(url, callback) {
+    var self = this;
+
+    request(url, function(error, response, html) {
+        var $, links = [];
+
+        if (!error) {
+            $ = cheerio.load(html);
+            $('a[href]').each(function(index) {
+                var $link = $(this),
+                    link,
+                    href = $link.attr('href');
+
+                link = {
+                    'href': href,
+                    'text': $link.text()
+                };
+                is_external = (link.href.indexOf('http') === 0 || link.href.indexOf('//') === 0);
+
+                process.nextTick(function() {
+                    linkType = is_external ? 'externalLink' : 'internalLink';
+                    self.emit(linkType, link);
+                });
+            });
+        }
+
+        process.nextTick(function() {
+            callback();
+        });
+    });
+}
+
 
 
 module.exports = {
