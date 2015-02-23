1d33383e8a90873589dd7a9a1d84651a27438a69
Mike Davies <isofarro2@gmail.com>
2015-02-23 08:23:00 +0000

    Initial starting point of a crawler job, hooked in EventEmitter.

---
+++ crawl.js
@@ -0,0 +1,35 @@
+var events = require('events');
+
+
+function Crawler() {
+    var self = this;
+    if (!(this instanceof Crawler)) {
+        return new Crawler();
+    }
+
+    events.EventEmitter.call(self);
+
+    process.nextTick(function() {
+        self.start();
+    });
+}
+
+Crawler.prototype.__proto__ = events.EventEmitter.prototype;
+
+Crawler.prototype.startUrl = function(startUrl) {
+    this.startUrl = startUrl;
+    return this;
+}
+
+Crawler.prototype.start = function() {
+    console.log("Starting crawler");
+    this.emit('externalLink', {href: 'http://www.google.com/'});
+    this.emit('internalLink', {href: 'http://www.mainlytea.com/'});
+}
+
+
+module.exports = {
+    Crawler: function() {
+        return new Crawler();
+    }
+}
