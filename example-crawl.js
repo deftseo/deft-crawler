bb390d0f478704528707bf3c31512f065c4ef23c
Mike Davies <isofarro2@gmail.com>
2015-02-23 08:58:11 +0000

    Revert example back to a find-external-links crawl.

--- example-crawl.js
+++ example-crawl.js
@@ -1,15 +1,10 @@
 var crawl = require('./crawl'),
     crawler = crawl.Crawler();
 
-
-
 crawler
     .startUrl('http://mainlytea.com/')
     .on('externalLink', function(link) {
         console.log("External Link: " + link.href);
-    })
-    .on('internalLink', function(link) {
-        console.log("Internal Link: " + link.href);
     });
 
 console.log("Crawler configured");
