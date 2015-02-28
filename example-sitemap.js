8ec8637ccf9b7b162319b07401fcbd1a85a6d83d
Mike Davies <isofarro2@gmail.com>
2015-02-28 20:34:11 +0000

    Add an array to track crawled internal links, so that we can generate a sitemap.

--- example-sitemap.js
+++ example-sitemap.js
@@ -1,9 +1,23 @@
 var crawl = require('./crawl'),
     crawler = crawl.Crawler();
 
+var sitemap = [];
+
 crawler
     .startUrl('http://lotsofyoga.com/')
     .on('internalLink', function(link) {
-        console.log(" * Internal: " + link.href);
+
+        if (link.statusCode === 200) {
+            console.log("[-MAP-] " + link.href);
+            sitemap.push(link.href);
+        } else {
+            console.log("\t[-" + link.statusCode + "-] " + link.href);
+        }
+    })
+    .on('linkError', function(link) {
+        console.log("[-" + link.statusCode + "-] " + link.href);
+    })
+    .on('end', function() {
+        console.log("[-END-] " + sitemap.length + " links");
     });
 
