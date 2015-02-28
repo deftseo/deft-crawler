1da9e40293a42866f1633055aff209cc26842ceb
Mike Davies <isofarro2@gmail.com>
2015-02-28 20:33:41 +0000

    More verbose logging from a simple site crawl.

--- example-crawl.js
+++ example-crawl.js
@@ -3,7 +3,13 @@ var crawl = require('./crawl'),
 
 crawler
     .startUrl('http://mainlytea.com/')
+    .on('internalLink', function(link) {
+        console.log("[INTER] " + link.href)
+    })
     .on('externalLink', function(link) {
-        console.log("[LINK-] External: " + link.href);
+        console.log("[EXTER] " + link.href);
+    })
+    .on('linkNotFound', function(link) {
+        console.log("[-" + link.statusCode + "-] " + link.href);
     });
 
