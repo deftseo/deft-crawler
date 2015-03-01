7e64822ee9febd9d2f58ddc1165fb66cb5ba56ed
Mike Davies <isofarro2@gmail.com>
2015-03-01 11:09:15 +0000

    Example of archive.org crawl

---
+++ example-archive.js
@@ -0,0 +1,27 @@
+var crawl = require('./crawl'),
+    crawler = crawl.Crawler(),
+    domain = "wiringschematics.net",
+
+    // Archive.org URL patterns
+    startUrl = 'https://web.archive.org/web/20120627105817/http://' + domain + '/',
+    archivePattern = "^https:\/\/web\.archive\.org\/web\/\\d+\/http:\/\/" + domain + "\/",
+    pagePattern = new RegExp(archivePattern),
+    maxDate = '20121231',
+    minDate = '20120101';
+
+crawler
+    .startUrl(startUrl)
+    .follow(function(link) {
+        isArchive = pagePattern.test(link);
+        if (isArchive) {
+            dateMatches = /(\d{6})\d+/.exec(link);
+            isArchive = (dateMatches[1] <= maxDate) && (dateMatches[1] >= minDate);
+        }
+        return isArchive;
+    })
+    .on('internalLink', function(link) {
+        //console.log("[INTER] " + link.href)
+    })
+    .on('linkError', function(link) {
+        console.log("[-" + link.statusCode + "-] " + link.href);
+    });
