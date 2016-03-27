8fd6be4b3c25cdc25b3bd3df0e09299a8ad93f5d
Mike Davies <isofarro2@gmail.com>
2016-03-27 12:45:58 +0100

    Create sitemap xml doc and write to filesystem.

--- examples/create-sitemap.js
+++ examples/create-sitemap.js
@@ -1,4 +1,5 @@
 var Crawler = require('../Crawler'),
+    fs = require('fs'),
     crawler = Crawler.Crawler(),
     args = process.argv.slice(2),
     startUrl = (args.length)? args[0] : 'http://example.com/';
@@ -24,5 +25,29 @@ crawler
 
         // TODO: Create a sitemap.xml from this
         console.log(sitemap);
+        createSiteMap(sitemap);
     });
 
+
+function createSiteMap(siteLinks, filename, frequency) {
+    var buffer = [],
+        xmlDoc;
+
+    frequency = frequency || 'daily';
+    filename = filename || 'sitemap.xml';
+
+    siteLinks.forEach(function(link) {
+        buffer.push("<url>\n"
+            + "\t<loc>" + link + "</loc>\n"
+            + "\t<changefreq>" + frequency + "</changefreq>\n"
+            + "</url>\n"
+        );
+    });
+
+    xmlDoc = '<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:schemaLocation="http://www.sitemaps.org/schemas/sitemap/0.9 http://www.sitemaps.org/schemas/sitemap/0.9/sitemap.xsd">\n'
+        + buffer.join('')
+        + '</urlset>\n';
+
+    console.log('Writing', siteLinks.length, 'sitemap links to:', filename);
+    fs.writeFileSync(filename, xmlDoc, 'utf-8');
+}
