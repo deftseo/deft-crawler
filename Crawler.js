6c2ea626cb1896c7282280fc128d891e81205292
DeftSEO <deftseo.inboxlite.com>
2017-08-11 03:36:34 -0400

    Mock up head/tail queue implementation as starting point for a PaginatedQueue.

--- Crawler.js
+++ Crawler.js
@@ -34,7 +34,37 @@ SimpleQueue.prototype.queueLength = function() {
 }
 
 
+/**********************************************************************
+*
+* PaginatedQueue -- a paginated queue so it can be persisted to disk
+*
+**********************************************************************/
+function PaginatedQueue() {
+    var self = this;
+    if (!(this instanceof PaginatedQueue)) {
+        return new PaginatedQueue();
+    }
 
+    this.items = [];
+    this.headIdx = 0;
+    this.tailIdx = 0;
+};
+
+PaginatedQueue.prototype.next = function() {
+    var item = this.items[this.headIdx];
+    this.items[this.headIdx] = null;
+    this.headIdx++;
+    return item;
+};
+
+PaginatedQueue.prototype.add = function(item) {
+    this.items[this.tailIdx] = item;
+    this.tailIdx++;
+}
+
+PaginatedQueue.prototype.queueLength = function() {
+    return this.tailIdx - this.headIdx;
+}
 
 
 /**********************************************************************
