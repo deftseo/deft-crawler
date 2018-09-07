e591b3d4624454f534298aeeba65ee8af84784ca
DeftSEO <deftseo@inboxlite.com>
2018-09-07 12:54:45 -0400

    Test with same domain and one different domain.

--- test/test-isSameDomain.js
+++ test/test-isSameDomain.js
@@ -11,7 +11,14 @@ describe('isSameDomain()', function() {
         ""
     ];
 
-    it('should compare two urls', function() {
-        assert.ok(isSameDomain(links[0], links[0], "same url matches"));
+    it('should equate URLs with same domain', function() {
+        assert.ok(isSameDomain(links[0], links[0]), "Same URL matches");
+        assert.ok(isSameDomain(links[0], links[1]), "Identical URL matches");
+        assert.ok(isSameDomain(links[0], links[2]), "URLs from same domain match");
+        assert.ok(isSameDomain(links[0], links[3]), "same domain, different protocol matches");
+    });
+
+    it("shouldn't match different domains", function() {
+        assert.ok(!isSameDomain(links[0], links[4]), "different domains");
     });
 });
