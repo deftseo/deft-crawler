3fc727d904b6a1eb14c9dd5745173e23a137238c
DeftSEO <deftseo@inboxlite.com>
2018-10-22 15:45:19 -0400

    create friendlier interface to a single-page scraper.

--- index.js
+++ index.js
@@ -5,6 +5,9 @@ var UrlUtils = require('./lib/url-utils');
 module.exports = {
     Crawler: crawler.Crawler,
     Scraper: scraper.Scraper,
+    scrape: function(scrapeUrl, scrapeFn) {
+        return this.Scraper(scrapeUrl, scrapeFn);
+    },
     isSameDomain: UrlUtils.isSameDomain,
     normaliseUrl: UrlUtils.normaliseUrl    
 };
