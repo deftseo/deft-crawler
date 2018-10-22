388cec24e353ffb34d670f5f7b534ef223f192aa
DeftSEO <deftseo@inboxlite.com>
2018-10-22 15:32:48 -0400

    Create consolidated package for a public interface.

---
+++ index.js
@@ -0,0 +1,10 @@
+var crawler = require('./Crawler');
+var scraper = require('./Scraper');
+var UrlUtils = require('./lib/url-utils');
+
+module.exports = {
+    Crawler: crawler.Crawler,
+    Scraper: scraper.Scraper,
+    isSameDomain: UrlUtils.isSameDomain,
+    normaliseUrl: UrlUtils.normaliseUrl    
+};
